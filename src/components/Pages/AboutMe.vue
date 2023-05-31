711d383925e4acb10e221cc7aa04034cd3ee81dd
Luis Felipe <202makertv@gmail.com>
2023-05-30 22:01:25 -0300

    Now its possible to add a lateral image on about me

--- src/components/Pages/AboutMe.vue
+++ src/components/Pages/AboutMe.vue
@@ -3,7 +3,16 @@
     <div class="container">
       <h2 class="text-center">Sobre mim <q-btn v-if="is_admin" @click="edit_about_me.modal = true" round color="white"
           class="q-mb-sm"><q-icon color="black" name="brush" /></q-btn></h2>
-      <p class="q-mt-md" v-html="about_me"></p>
+      <div class="flex justify-between">
+        <div :class="{ w50: has_image }">
+          <p class="q-mt-md" v-html="about_me"></p>
+        </div>
+        <div class="w50 relative-position" v-if="has_image">
+          <q-img :src="image_about_me" />
+          <q-btn class="absolute-right remove-image-button" v-if="is_admin" @click="removeImage" round color="red"><q-icon
+              name="remove" /></q-btn>
+        </div>
+      </div>
     </div>
 
     <q-dialog v-model="edit_about_me.modal">
@@ -14,6 +23,12 @@
 
             <h4 class="text-center text-black">Editar sobre mim</h4>
             <q-editor class="about-me-edit" v-model="edit_about_me.text" min-height="5rem" />
+            <q-file accept=".jpg, .png, .webp, .jpeg, image/*" v-model="edit_about_me.image" label="Imagem lateral" filled
+              :clearable="true" :filter="checkFile" @rejected="onRejected">
+              <template v-slot:prepend>
+                <q-icon name="attach_file" />
+              </template>
+            </q-file>
 
             <div>
               <q-btn label="Editar" type="submit" color="primary" />
@@ -27,11 +42,12 @@
 
 <script setup>
 import { useSiteInfoStore } from 'stores/siteInfoStore.js'
-import { ref, onMounted, defineProps, watch } from 'vue'
+import { ref, defineProps, watch, computed, onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
+import { IMAGES_PATH } from 'src/shared/helpers';
 
 const site_info = useSiteInfoStore()
-const { about_me } = storeToRefs(site_info)
+const { about_me, image } = storeToRefs(site_info)
 const props = defineProps({
   is_admin: {
     type: Boolean,
@@ -43,21 +59,65 @@ const temp_about_me = ref('')
 const edit_about_me = ref({
   modal: false,
   text: temp_about_me,
+  image: '',
 })
 
 onMounted(() => {
   temp_about_me.value = site_info.about_me
-
 })
 
 watch(about_me, (new_value) => {
   if (new_value) {
-    temp_about_me.value = new_value
+    edit_about_me.value.text = new_value
   }
 })
 
+const image_about_me = computed(() => {
+  if (image && image.value) return `${IMAGES_PATH()}/${image.value}`
+  return ''
+})
+
+const has_image = computed(() => {
+  if (image && image.value && image.value.length > 0) return true
+  return false
+})
+
 async function changeAboutMe() {
-  if (await site_info.editAboutMe(edit_about_me.value.text)) site_info.about_me = edit_about_me.value.text
+  const formData = new FormData()
+
+  formData.append('about_me', edit_about_me.value.text)
+  formData.append('image', edit_about_me.value.image)
+
+  const created = await site_info.editAboutMe(formData)
+  if (!created) return
+
+  await site_info.getSiteInfo()
+}
+
+async function removeImage() {
+  const deleted = await site_info.deleteAboutMeImage()
+  if (!deleted) return
+
+  await site_info.getSiteInfo()
+}
+
+function checkFile(files) {
+  const allow_types = [
+    'image/jpeg',
+    'image/png',
+    'image/jpg',
+    'image/jpeg',
+    'image/webp',
+  ]
+  return files.filter(file => file.size < 1024 * 1000 && (allow_types.includes(file.type)))
+}
+
+function onRejected(rejectedEntries) {
+
+  Notify.create({
+    type: 'negative',
+    message: `Arquivo inválido! Verifique se o arquivo não ultrapassa o tamanho máximo (1 mb) e se ele é um formato válido de imagem`
+  })
 }
 
 </script>
@@ -81,6 +141,11 @@ async function changeAboutMe() {
 }
 </style>
 <style lang="scss" scoped>
+.remove-image-button {
+  width: 48px;
+  height: 48px;
+}
+
 .buttons {
   justify-content: right;
 
--- src/stores/siteInfoStore.js
+++ src/stores/siteInfoStore.js
@@ -16,11 +16,13 @@ const ENDPOINTS = {
   edit_about_me: '/edit_about_me',
   edit_logo: '/edit_logo',
   delete_video: '/video',
+  delete_about_me_image: '/about_me_image',
 }
 
 export const useSiteInfoStore = defineStore('siteInfo', {
   state: () => ({
     about_me: '',
+    image: '',
     logo: '',
   }),
   actions: {
@@ -29,8 +31,11 @@ export const useSiteInfoStore = defineStore('siteInfo', {
       return await api.get(`${ENDPOINTS.get_site_info}`)
         .then(response => {
           if(!response.data.success) throw "Não foi possível recuperar os dados dessa página"
+          console.log("RESPONSE: ", response.data.result)
           this.about_me = response.data.result[0].about_me
+          this.image = response.data.result[0].about_me_image
           this.logo = response.data.result[0].logo
+          console.log("IMAGE: ", this.image)
 
           return response.data.result[0]
           }
@@ -58,21 +63,27 @@ export const useSiteInfoStore = defineStore('siteInfo', {
       });
     },
 
-    async editAboutMe(about_me) {
-      return await api.patch(`${ENDPOINTS.edit_about_me}`, {
-        about_me: about_me,
-      })
-        .then(response => {
+    async editAboutMe(data) {
+      return await axios({
+        method: 'post',
+        url: `${API_PATH()}site_info${ENDPOINTS.edit_about_me}`,
+        data: data,
+        headers: {
+          'Authorization': LocalStorage.getItem('user_token') || 0,
+          'Content-Type': 'multipart/form-data'
+        }
+      }).then(response => {
           if(!response.data.success) throw `Não foi possível editar o texto`
+
           positiveNotification(`Sobre mim editado com sucesso`)
           return response.data.success
-          }
-        )
+        })
         .catch(e => {
           if (e.response) negativeNotification(e.response.data.error)
           else negativeNotification(`Não foi possível editar o texto`)
           throw e;
-      });
+        });
+
     },
 
     async editLogo(data) {
@@ -110,6 +121,21 @@ export const useSiteInfoStore = defineStore('siteInfo', {
         .catch(e => {
           if (e.response) negativeNotification(e.response.data.error)
           else negativeNotification(`Não foi possível deletar o vídeo`)
+          throw e;
+      });
+    },
+
+    async deleteAboutMeImage() {
+      return await api.delete(`${ENDPOINTS.delete_about_me_image}`)
+        .then(response => {
+          if(!response.data.success) throw `Não foi possível deletar a imagem lateral do sobre nós`
+          positiveNotification(`Imagem lateral deletada com sucesso`)
+          return response.data.success
+          }
+        )
+        .catch(e => {
+          if (e.response) negativeNotification(e.response.data.error)
+          else negativeNotification(`Não foi possível deletar a imagem lateral`)
           throw e;
       });
     },
